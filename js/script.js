c5a70f54b8462801b12ffd277b66fa2bfefe3595
talita-aya <tatakuriki@gmail.com>
2021-10-15 14:09:47 -0300

    animações

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-new SimpleSlide({
-  slide: "quote", // nome do atributo data-slide="principal"
-  time: 5000, // tempo de transição dos slides
-});
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+  });
 
-new SimpleSlide({
-  slide: "portfolio", // nome do atributo data-slide="principal"
-  time: 5000, // tempo de transição dos slides
-  nav: true,
-});
+  new SimpleSlide({
+    slide: "portfolio", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
+    nav: true,
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
